cb287b72163fded6b1f35048d8279be0a6b7c267
Michael J Long <michael.long@greymatter.io>
2022-03-14 09:24:19 -0400

    Adds another partial function example

--- util/partial.go
+++ util/partial.go
@@ -1,10 +1,19 @@
 package util
 
-//Takes a function of two parameters and turns it into a function with one parameter with the first parameter bound.
+//Takes a function of two arguments and turns it into a function with one argument with the first argument bound.
 //This is useful replacement for a Class with instance variables in OOP.
 func Partial1[T1, T2, T3 any](f func(T1, T2) T3, s1 T1) func(T2) T3 {
 	return func(s2 T2) T3 {
 		return f(s1, s2)
+	}
+}
+
+//Takes a function of three arguments and turns it into a function with two arguments with the first argument bound.
+//This is useful replacement for a Class with instance variables in OOP.
+//TODO I wonder if there is a way to make the function variadic and still preserve generic type safety?  I don't think so because type parameters cannot be variadic.
+func Partial2[T1, T2, T3, T4 any](f func(T1, T2, T3) T4, s1 T1) func(T2, T3) T4 {
+	return func(s2 T2, s3 T3) T4 {
+		return f(s1, s2, s3)
 	}
 }
 
--- util/partial_test.go
+++ util/partial_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -12,6 +13,20 @@ func TestPartial1(t *testing.T) {
 	var f = Partial1(p, "Hello")
 	expected := "Hello World"
 	var actual = f(" World")
+	if actual != expected {
+		t.Errorf("actual:%v, expected:%v", actual, expected)
+
+	}
+}
+
+func TestPartial2(t *testing.T) {
+	p := func(s1 string, s2 string, s3 int) string {
+		return s1 + s2 + fmt.Sprintf("%v", s3)
+	}
+
+	var f = Partial2(p, "Hello")
+	expected := "Hello World-12"
+	var actual = f(" World-", 12)
 	if actual != expected {
 		t.Errorf("actual:%v, expected:%v", actual, expected)
 
